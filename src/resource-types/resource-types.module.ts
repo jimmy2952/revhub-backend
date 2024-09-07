ecad2f402edcf2c82bf683dbe2ccec8428ff2c3d
Jimmy Chen <jimmy2952@gmail.com>
2024-09-07 10:44:33 +0800

    refactor: adding sharedModule

--- src/resource-types/resource-types.module.ts
+++ src/resource-types/resource-types.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { SharedModule } from 'src/shared.module'
 import { ResourceTypesController } from './resource-types.controller'
 import { ResourceTypesService } from './resource-types.service'
-import { PrismaService } from 'src/prisma.service'
 
 @Module({
+  imports: [SharedModule],
   controllers: [ResourceTypesController],
-  providers: [ResourceTypesService, PrismaService],
+  providers: [ResourceTypesService],
 })
 export class ResourceTypesModule {}
