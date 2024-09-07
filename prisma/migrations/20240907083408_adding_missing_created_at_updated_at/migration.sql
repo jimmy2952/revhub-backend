ad47593cc34821244f193fde403ac9d138996900
Jimmy Chen <jimmy2952@gmail.com>
2024-09-07 16:34:45 +0800

    feat: adding createdAt and updatedAt

---
+++ prisma/migrations/20240907083408_adding_missing_created_at_updated_at/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Event` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `EventItem` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `EventItemsOnResources` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Like` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `ResourceType` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Review` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Event" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "EventItem" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "EventItemsOnResources" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Like" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "ResourceType" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Review" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
